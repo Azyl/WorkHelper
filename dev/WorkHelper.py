6dd6566bc99a2c8b7457835390e2817a9978e07e
Andrei Emanuel Tataru <tataru.andrei.emanuel@gmail.com>
2012-04-23 15:16:57 +0300

    aded stable v0.01 and dev v0.02

---
+++ dev/WorkHelper.py
@@ -0,0 +1,15 @@
+'''
+Created on Apr 16, 2012
+
+@author: emy
+'''
+
+if __name__ == '__main__':
+    from Tkinter import *
+
+    root = Tk()
+
+    w = Label(root, text="Hello, world!")
+    w.pack()
+
+    root.mainloop()
