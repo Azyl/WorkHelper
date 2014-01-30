106b37ea9e6deb99426a57fd8a0696ae4da96aab
Tataru Andrei <tataru.andrei.emanuel@gmail.com>
2014-01-30 10:00:10 +0200

    Update updater.py
    updated the script to be more oo, added a logger

--- dev/updater.py
+++ dev/updater.py
@@ -6,133 +6,134 @@ Created on Jan 29, 2014
 '''
 
 
-
 import sys, os, re, ftplib
 import hashlib
+import logging
 from optparse import OptionParser
 
-#config = os.path.expanduser('~/.nftprc')
-config = os.path.expanduser('./updater.config')
-r_field = re.compile(r'(?s)([^\n:]+): (.*?)(?=\n[^ \t]|\Z)')
 
-def getConfig(name=None):
-    # Find the config file
-    home = os.path.expanduser('~/')
-    nftp_conf = os.getenv('NFTP_CONF')
-    if nftp_conf is not None: 
-        s = open(config).read()
-    elif os.path.exists(config): 
-        s = open(config).read()
-    elif os.path.exists('.nftprc'): 
-        s = open('.nftprc').read()
-    elif os.path.exists('nftp.conf'): 
-        s = open('nftp.conf').read()
-    elif os.path.exists(home + 'nftp.conf'): 
-        s = open(home + 'nftp.conf').read()
-    else:
-        return {}
-    conf = {}
-    s = s.replace('\r\n', '\n')
-    s = s.replace('\r', '\n')
-    for item in s.split('\n\n'): 
-        meta = dict(r_field.findall(item.strip()))
-        if meta.has_key('name'): 
-            fname = meta['name']
-            del meta['name']
-            conf[fname] = meta
-        else: raise 'ConfigError', 'Must include a name'
 
-    if name is not None: 
-        return conf[name]
-    else: return conf
 
-def getFtp(meta): 
-    ftp = ftplib.FTP(meta['host'], meta['username'], meta['password'])
-    print 'Changing remote dir (ftp) to: ' + meta['remotedir']
-    ftp.cwd(meta['remotedir'])
-    return ftp
-
-def getMD5(file):
-    hash = hashlib.md5(file.read()).hexdigest()
-    return hash
-
-def checkDirectory(meta):
-    ftp=getFtp(meta)
-    #files = ftp.dir()
-    #print files
+class updater():
+    def __init__(self):
+        
+        #logging:
+        self.lgr = logging.getLogger('updater')
+        self.lgr.setLevel(logging.DEBUG)
+        #fh = logging.FileHandler('./myapp.log')
+        self.fh = logging.StreamHandler()
+        self.fh.setLevel(logging.DEBUG)
+        frmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.fh.setFormatter(frmt)
+        self.lgr.addHandler(self.fh)
+     
+        #config:
+        self.meta=self.getConfig()
     
-    filelist=ftp.nlst()
-    print "files in the remote folder: "
-    for file in filelist:
-        print "    "+file
-        print "checking if the file exists in the localdirectory:"
-        try:
-            with open(meta['localdir']+'\\' +file) as f:
-                
-                localMD5 = getMD5(f)
-                print ('file '+file+' exists locally')
-                print '    local file MD5:  '+localMD5
-                filetmp = open(meta['localdir']+'\\' + file + '.tmp', 'wb')
+    def getConfig(self,name=None):
+        
+        #config = os.path.expanduser('~/.nftprc')
+        config = os.path.expanduser('./updater.config')
+        r_field = re.compile(r'(?s)([^\n:]+): (.*?)(?=\n[^ \t]|\Z)')
+        # Find the config file
+        home = os.path.expanduser('~/')
+        nftp_conf = os.getenv('NFTP_CONF')
+        if nftp_conf is not None: 
+            s = open(config).read()
+        elif os.path.exists(config): 
+            s = open(config).read()
+        elif os.path.exists('.nftprc'): 
+            s = open('.nftprc').read()
+        elif os.path.exists('nftp.conf'): 
+            s = open('nftp.conf').read()
+        elif os.path.exists(home + 'nftp.conf'): 
+            s = open(home + 'nftp.conf').read()
+        else:
+            return {}
+        conf = {}
+        s = s.replace('\r\n', '\n')
+        s = s.replace('\r', '\n')
+        for item in s.split('\n\n'): 
+            meta = dict(r_field.findall(item.strip()))
+            if meta.has_key('name'): 
+                fname = meta['name']
+                del meta['name']
+                conf[fname] = meta
+            else:
+                raise 'ConfigError', 'Must include a name'
+                self.lgr.error('Config error, Config file does not conatin a name field')
+        if name is not None: 
+            return conf[name]
+        else: return conf
+
+    def getFtp(self,meta): 
+        ftp = ftplib.FTP(meta['host'], meta['username'], meta['password'])
+        self.lgr.info('Changing remote dir (ftp) to: ' + meta['remotedir'])
+        ftp.cwd(meta['remotedir'])
+        return ftp        
+
+    def getMD5(self,file):
+        hash = hashlib.md5(file.read()).hexdigest()
+        return hash
+
+    def checkDirectory(self,meta):
+        ftp=self.getFtp(meta)
+        #files = ftp.dir()
+        #print files
+    
+        filelist=ftp.nlst()
+        self.lgr.info('files in the remote folder: ')
+        for file in filelist:
+            self.lgr.info('    '+file)
+            self.lgr.info('checking if the file exists in the localdirectory:')
+            try:
+                with open(meta['localdir']+'\\' +file) as f:
+                    localMD5 = self.getMD5(f)
+                    self.lgr.info('file '+file+' exists locally')
+                    self.lgr.info('    local file MD5:  '+localMD5)
+                    filetmp = open(meta['localdir']+'\\' + file + '.tmp', 'wb')
+                    ftp.retrbinary('RETR '+meta['remotedir']+'/' + file, filetmp.write);
+                    filetmp.close()
+                    filex=open(meta['localdir']+'\\' + file + '.tmp', 'r')
+                    remoteMD5 = self.getMD5(filex)
+                    filex.close()
+                    self.lgr.info('    remote file MD5: '+remoteMD5)
+                    if localMD5==remoteMD5:
+                        self.lgr.info('file '+file+' is up to date')
+                        os.remove(meta['localdir']+'\\' + file + '.tmp')
+                    else:
+                        os.remove(meta['localdir']+'\\' + file)
+                        os.rename(meta['localdir']+'\\' + file + '.tmp', meta['localdir']+'\\' + file)
+            except IOError:
+                self.lgr.info('file ' +file+' does not exist locally')
+                filetmp = open(meta['localdir']+'\\' + file, 'wb')
                 ftp.retrbinary('RETR '+meta['remotedir']+'/' + file, filetmp.write);
                 filetmp.close()
-                filex=open(meta['localdir']+'\\' + file + '.tmp', 'r')
-                remoteMD5 = getMD5(filex)
-                filex.close()
-                print '    remote file MD5: '+remoteMD5
-                if localMD5==remoteMD5:
-                    print 'file '+file+' is up to date'
-                    os.remove(meta['localdir']+'\\' + file + '.tmp')
-                else:
-                    os.remove(meta['localdir']+'\\' + file)
-                    os.rename(meta['localdir']+'\\' + file + '.tmp', meta['localdir']+'\\' + file)
-        except IOError:
-            print 'file ' +file+' does not exist locally'
-            filetmp = open(meta['localdir']+'\\' + file, 'wb')
-            ftp.retrbinary('RETR '+meta['remotedir']+'/' + file, filetmp.write);
-            filetmp.close()
-            print 'file '+file+ ' downloaded'
-        
-        
-        
-        
-     
-   
-    
+                self.lgr.info('file '+file+ ' downloaded')
 
-def getFile(meta,filename,tmp):
-    ftp=getFtp(meta)
-    file = open(meta['localdir']+'\\' + filename + tmp, 'wb')
-    print 'Opening local file ' 
-    ftp.retrbinary('RETR '+meta['remotedir']+'/' + filename, file.write);
-    print 'Closing file ' + filename
-    ftp.quit()
+        
+    def getFile(self,meta,filename,tmp):
+        ftp=self.getFtp(meta)
+        file = open(meta['localdir']+'\\' + filename + tmp, 'wb')
+        print 'Opening local file ' 
+        ftp.retrbinary('RETR '+meta['remotedir']+'/' + filename, file.write);
+        print 'Closing file ' + filename
+        ftp.quit()
 
 def main(argv=None):
-    #meta={'host':'ftp.crisoft.ro','username':'lemet','password':'lemetftp1','remotedir':'/HealthCheck/updater'}
-    #meta={'host':['ftp.crisoft.ro'],'username':['lemet'],'password':['lemetftp1'],'remotedir':['/HealthCheck/updater']}
+
+    Refresher = updater()
+    Refresher.lgr.info('the following directories have been configured:')
+    for key in Refresher.meta:
+        Refresher.lgr.info(" -- " + key)
+        for k,v in Refresher.meta[key].iteritems():
+            Refresher.lgr.info(k+":"+v)
+
     
-    #getFtp(meta)
-    #get('Install.sql','/HealthCheck/updater')
-    
-    #meta = getConfig('example')
-    
-    meta=getConfig()
-    print "the following directories have been configured:"
-    for key in meta:
-        print " -- " + key
-        for k,v in meta[key].iteritems():
-            print k+":"+v
-    
-    for key in meta:
-        print " ++ " + key
-        #getFile(meta[key],'Install.sql')
-        checkDirectory(meta[key])
-        
-        
-        
-    #print meta
-    #getFtp(meta['example'])
-    #get('example','/HealthCheck/updater')
+    for key in Refresher.meta:
+        Refresher.lgr.info(" ++ " + key)
+        Refresher.checkDirectory(Refresher.meta[key])
+
 
 if __name__=="__main__": 
     main()
